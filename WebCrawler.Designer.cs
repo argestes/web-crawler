507a1f34f8721e19f9a4a4d7247d84ecb5864452
Yigit Unlu <yigitunlu@gmail.com>
2013-03-11 01:42:55 +0200

    Multithread, reconnection

--- WebCrawler.Designer.cs
+++ WebCrawler.Designer.cs
@@ -28,25 +28,87 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.htmlBox = new System.Windows.Forms.RichTextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.threadCountLabel = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // htmlBox
             // 
-            this.htmlBox.Location = new System.Drawing.Point(21, 12);
+            this.htmlBox.AccessibleRole = System.Windows.Forms.AccessibleRole.Grip;
+            this.htmlBox.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.htmlBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.htmlBox.Location = new System.Drawing.Point(0, 0);
+            this.htmlBox.MaximumSize = new System.Drawing.Size(2000, 2000);
             this.htmlBox.Name = "htmlBox";
-            this.htmlBox.Size = new System.Drawing.Size(384, 226);
+            this.htmlBox.ReadOnly = true;
+            this.htmlBox.Size = new System.Drawing.Size(587, 262);
             this.htmlBox.TabIndex = 0;
             this.htmlBox.Text = "";
+            this.htmlBox.WordWrap = false;
+            this.htmlBox.TextChanged += new System.EventHandler(this.htmlBox_TextChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.htmlBox);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(587, 262);
+            this.panel1.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.threadCountLabel);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 238);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(587, 24);
+            this.panel2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Thread Count:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // threadCountLabel
+            // 
+            this.threadCountLabel.AutoSize = true;
+            this.threadCountLabel.Location = new System.Drawing.Point(120, 6);
+            this.threadCountLabel.Name = "threadCountLabel";
+            this.threadCountLabel.Size = new System.Drawing.Size(13, 13);
+            this.threadCountLabel.TabIndex = 1;
+            this.threadCountLabel.Text = "0";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // WebCrawler
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(433, 262);
-            this.Controls.Add(this.htmlBox);
+            this.ClientSize = new System.Drawing.Size(587, 262);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
             this.Name = "WebCrawler";
             this.Text = "Web Crawler";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -54,6 +116,11 @@
         #endregion
 
         private System.Windows.Forms.RichTextBox htmlBox;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label threadCountLabel;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- WebCrawler.cs
+++ WebCrawler.cs
@@ -8,12 +8,24 @@ using System.Text;
 using System.Windows.Forms;
 using HtmlAgilityPack;
 using System.Threading;
-
+using System.Web;
+using System.IO;
+using System.Diagnostics;
 namespace web_crawler
 {
     public partial class WebCrawler : Form
     {
         delegate void SetTextCallback(string text);
+        
+        FileStream fs;
+        StreamWriter sw;
+        private static string DecodeUrlString(string url)
+        {
+            string newUrl;
+            while ((newUrl = Uri.UnescapeDataString(url)) != url)
+                url = newUrl;
+            return newUrl;
+        }
         private void AddText(string text)
         {
 
@@ -27,61 +39,162 @@ namespace web_crawler
             }
             else
             {
-                this.htmlBox.Text += text;
+               // this.htmlBox.Text += text;
+                this.sw.Write(text);
+                this.sw.Flush();
             }
         }
 
         private void CrawlNamePage(Uri uri, String link)
         {
-            HtmlWeb web = new HtmlWeb();
-            HtmlAgilityPack.HtmlDocument subDoc = new HtmlAgilityPack.HtmlDocument();
-
-            subDoc = web.Load(uri.Scheme + "://" + uri.Host + link);
-            
-            HtmlNode name = subDoc.GetElementbyId("firstHeading");
-            String word = name.SelectNodes(".//span").First().InnerText;
-            HtmlNodeCollection wordMeaningNodes = subDoc.DocumentNode.SelectNodes("//dl//dd");
-
-            AddText(word + ": \n");
-            foreach (HtmlNode meaningNode in wordMeaningNodes)
+            try
             {
-                AddText(meaningNode.InnerText + "\n");
+                //AddText("Crawling Name Page: " + uri + "\n\r");
+                HtmlWeb web = new HtmlWeb();
+                HtmlAgilityPack.HtmlDocument subDoc = new HtmlAgilityPack.HtmlDocument();
+                Boolean processed = false;
+
+                while (!processed)
+                {
+                    try
+                    {
+                        subDoc = web.Load(uri.Scheme + "://" + uri.Host + link);
+                        processed = true;
+                    }
+                    catch (Exception e)
+                    {
+                        AddText("reconnecting \n");
+                    }
+                }
+
+                HtmlNode name = subDoc.GetElementbyId("firstHeading");
+                String word = name.SelectNodes(".//span").First().InnerText;
+                HtmlNodeCollection wordMeaningNodes = subDoc.DocumentNode.SelectNodes("//dl//dd");
+                String wordAndMeanings = "";
+                wordAndMeanings += word + ": \n";
+                foreach (HtmlNode meaningNode in wordMeaningNodes)
+                {
+                    wordAndMeanings += meaningNode.InnerText + "\n";
+                }
+
+                AddText(wordAndMeanings);
+            }
+            catch (Exception e)
+            {
+                AddText(e.StackTrace);
             }
         }
-        List<String> FindTopPages(String firstPageUri,String nextPageLinkValue)
-        {
-            List<String> topPages = new List<string>();
-            Uri uri = new Uri(firstPageUri);
-            topPages.Add(uri.LocalPath);
-            HtmlWeb web = new HtmlWeb();
-            HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
 
-            return topPages;
-        }
-        public WebCrawler()
+        /// <summary>   Searches for the first names. </summary>
+        ///
+        /// <param name="topPagePath">  Full pathname of the top page html. </param>
+
+        void FindNames(String topPagePath)
         {
-            InitializeComponent();
             HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
-            Uri uri = new Uri("http://tr.wiktionary.org/wiki/Kategori:Soyad%C4%B1_(T%C3%BCrk%C3%A7e)");
             HtmlWeb web = new HtmlWeb();
-            doc = web.Load(uri.OriginalString);
-            htmlBox.Text += "Host:" + uri.Host + "\n";
-            htmlBox.Text += "Local path:" + uri.LocalPath + "\n";
+            doc = web.Load(topPagePath);
+            Uri topPageUri = new Uri(topPagePath);
             HtmlNode node = doc.GetElementbyId("mw-pages");
-
-            HtmlNodeCollection nextPageNodes = doc.DocumentNode.SelectNodes("//a[. = \"sonraki 200\"]");
-            foreach(HtmlNode nextPageNode in nextPageNodes)
-            htmlBox.Text += "Sonraki = " + nextPageNode.GetAttributeValue("href","/") + "\n";
 
             HtmlNodeCollection nameListNodes = node.SelectNodes(".//li");
             foreach (HtmlNode listNode in nameListNodes)
             {
                 HtmlNode aNode = listNode.SelectNodes(".//a[@href]").First();
                 String link = aNode.GetAttributeValue("href", "/");
-                //  htmlBox.Text += uri.Scheme +"://" + uri.Host + link + "\n";
+                Uri uri = new Uri(topPageUri.Scheme + "://" + topPageUri.Host + HttpUtility.HtmlDecode(link));
                 Thread myThread = new Thread(() => this.CrawlNamePage(uri,link));
+                //CrawlNamePage(uri, link);
                 myThread.Start();
             }
+        }
+
+        /// <summary>   Searches for the top pages. </summary>
+        ///
+        /// <param name="firstPageUri">         URI of the first page. </param>
+        /// <param name="nextPageLinkValue">    The next page link value. </param>
+        ///
+        /// <returns>   The found top pages. </returns>
+
+        List<String> FindTopPages(String firstPageUri,String nextPageLinkValue)
+        {
+            AddText("Top Pages Working");
+            /// <summary>   The top pages. </summary>
+            List<String> topPages = new List<string>();
+            /// <summary>   URI of the document. </summary>
+            Uri uri = new Uri(firstPageUri);
+            topPages.Add(uri.LocalPath);
+            HtmlWeb web = new HtmlWeb();
+            HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+            doc = web.Load(uri.OriginalString);
+            //doc.DocumentNode.SelectSingleNode("//a[. = \"sonraki 200\"]");
+           // AddText(nextPageXPath);
+           // AddText(doc.DocumentNode.InnerHtml);
+            HtmlNode nextPageNode = doc.DocumentNode.SelectSingleNode("//a[. = \"sonraki 200\"]");
+           
+            while (nextPageNode != null)
+            {
+                String topPage = nextPageNode.GetAttributeValue("href", "/");
+                topPages.Add(topPage);
+                String requestUri = uri.Scheme + "://" + uri.Host +HttpUtility.HtmlDecode(topPage) + "\n";
+                HtmlAgilityPack.HtmlDocument doc2 = web.Load(requestUri);
+                
+                AddText("Toppage URI:" + requestUri);
+                
+                nextPageNode = doc2.DocumentNode.SelectSingleNode("//a[. = \"sonraki 200\"]");
+                
+            }
+
+            foreach (String topPage in topPages)
+            {
+                Thread worker = new Thread(() => this.FindNames(uri.Scheme + "://" + uri.Host + HttpUtility.HtmlDecode(topPage)));
+
+                worker.Start();
+            }
+            return topPages;
+        }
+        public WebCrawler()
+        {
+            InitializeComponent();
+
+            fs = new FileStream("logfile.txt", FileMode.Create, FileAccess.ReadWrite);
+            if (fs == null)
+                htmlBox.Text = "File Stream Error!!";
+            else
+            {
+                sw = new StreamWriter(fs);
+                if (sw != null)
+                {
+                    AddText("INIT");
+                    Thread crawlerThread = new Thread(() => this.Crawl());
+                    crawlerThread.Start();
+                }
+            }
+        }
+        private void Crawl(){
+            AddText("Crawling");
+            HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+            Uri uri = new Uri("http://tr.wiktionary.org/wiki/Kategori:Soyad%C4%B1_(T%C3%BCrk%C3%A7e)");
+            HtmlWeb web = new HtmlWeb();
+
+            FindTopPages(uri.OriginalString, "sonraki 200");
+        }
+
+        private void htmlBox_TextChanged(object sender, EventArgs e)
+        {
+            
+            htmlBox.ScrollToCaret();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+           // threadCountLabel.Text += "aa";
+            threadCountLabel.Text= Process.GetCurrentProcess().Threads.Count.ToString();
         }
     }
 }
